4b8cd9b73edb0de0ca68667636dc0a34a27eb38f
Kaelan Davis <kaelandavis01@gmail.com>
2023-09-09 21:39:54 -0700

    implement routing

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -16,10 +16,24 @@ export class PokemonListComponent implements OnInit {
     this.getPokemonList();
   }
 
-  getPokemonList() {
+  /**
+   * Uses the PokemonService to populate pokemonList with information. 
+   */
+  private getPokemonList() {
     this.pokemonService.getPokemonList(1, 151)
       .subscribe(response => {
         this.pokemonList = response.results;
       });
+  }
+
+  /**
+   * Takes the endpoint for a specific pokemon and returns the id portion of the string.
+   *  
+   * @param url The pokemon url to extract the id from
+   * @returns The pokemon id
+   */
+  extractPokemonId(url:string): string {
+    const parts = url.split('/');
+    return parts[parts.length - 2];
   }
 }
